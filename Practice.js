59c5f136db3244d2265d83022ba0245f9b447514
Zehra232006 <mehdizehra31@gmail.com>
2024-10-13 20:42:12 +0530

    chnge

--- Practice.js
+++ Practice.js
@@ -1,3 +1,8 @@
-console.log("Helow Word");
-alert("Hey Im Mehdi");
-document.write("wordd");
+console.log("hey this is akbar");
+
+var a = 5;
+var b = 6;
+var c = "Akbar"
+console.log(a + b + 8);
+
+console.log(typeof a, typeof b, typeof c);
