cd23b4f0080322bfc4ffe61617f36eb16fd4257c
Zehra232006 <mehdizehra31@gmail.com>
2024-10-14 20:21:29 +0530

    javascript

---
+++ JavasScript/conditions.js
@@ -0,0 +1,22 @@
+num = 2134;
+
+if (num % 2 === 0) {
+
+    j = 'something'
+
+    console.log('Even');
+} else {
+    console.log('Odd');
+    
+}
+
+console.log(j);
+
+const age = 18;
+
+if (age >= 18) {
+    console.log("Eligible");
+} else {
+    console.log("Not Eligible");
+}
+
---
+++ JavasScript/index.js
@@ -0,0 +1,24 @@
+console.log("Hey Im Akbar");
+
+a = 10;
+console.log(a);
+
+msg = "bbye";
+console.log(msg);
+
+console.log(typeof a);
+console.log(typeof msg);
+
+isHungry = true;
+console.log(typeof isHungry);
+
+console.log(1 == 1);
+console.log('1' == 1);
+console.log('1' === 1);
+
+
+console.log(2 ** 4);
+console.log('1' + 1);
+console.log('1' + true);
+console.log('1' + 1 + 1);
+console.log(1 + 1 + '1');
